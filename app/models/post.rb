6ec888c5e1ad40db1bfd5bb1db909c7c52c4dfb2
nolyoi <nolan@moonship.cc>
2020-07-04 07:13:51 -0500

    Added pagination settings. Preparing for infite  scroll.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  self.per_page = 10
+  
   belongs_to :user
   belongs_to :community, optional: true
   belongs_to :parent, class_name: "Post", optional: true
@@ -18,7 +20,7 @@ class Post < ApplicationRecord
          email_allowed: :custom_notification_email_to_users_allowed?,
          notifiable_path: :custom_notifiable_path
 
-  before_save :convert_to_markdown 
+  before_save :convert_to_markdown
 
   private
 
