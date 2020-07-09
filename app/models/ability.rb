d66b44a880b1eddc05132740d86d7faaf8b24bcb
nolyoi <nolan@moonship.cc>
2020-07-09 05:37:49 -0500

    Added Message perms

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,17 @@ class Ability
   def initialize(user)
     can :read, Post, public: true
     can :read, User
+    cannot :read, Message
 
     if user.present?  # additional permissions for logged in users (they can read their own posts)
       can :read, Post, user_id: user.id
-      can :dashboard, User
+
       can :manage, User, id: user.id
+      can :dashboard, User
       can :read, User
+
+      can :manage, Message, to_id: user.id
+      can :manage, Message, from_id: user.id
 
       # if user.admin?  # additional permissions for administrators
       #   can :read, Post
