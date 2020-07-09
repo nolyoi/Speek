d1c5f12ea69a142835d49b219b19ba15f1cea7ed
nolyoi <nolan@moonship.cc>
2020-07-09 05:37:31 -0500

    Deleted unused methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
-module ApplicationHelper
+# frozen_string_literal: true
 
+module ApplicationHelper
   def hundred_char_limit(text)
     truncate(strip_tags(text), length: 100)
   end
-  
+
   private
 
   def current_user
@@ -12,53 +13,35 @@ module ApplicationHelper
 
   def current_ability
     @current_ability ||= Ability.new(current_user)
-  end  
+  end
 
   def require_login
-    redirect_to login_path,
-      {warning: 'You must be logged in access that page'} unless
-      session.include? :user_id
+    unless session.include? :user_id
+      redirect_to login_path,
+                  { warning: 'You must be logged in access that page' }
+    end
   end
 
   def is_shop_owner?
     if current_user
       current_user.id == @shop.owner_id
     else
-      false 
+      false
     end
   end
 
-  # def brightness(pixel)
-  #   Math.sqrt(
-  #     0.299 * pixel.red**2 +
-  #     0.587 * pixel.green**2 +
-  #     0.114 * pixel.blue**2
-  #   ).to_i
-  # end
-
-  # def image_brightness(path)
-  #   image = MiniMagick::Image.open(path)[0]
-  #   binding.pry
-  #   image.get_pixels do |pixel, c, r|
-  #     pixel.red = pixel.green = pixel.blue = brightness(pixel)
-  #     image.pixel_color(c, r, pixel)
-  #   end
-  # end
-
-
   def destroy_user_data(user_id, hash)
     case hash[:what]
-      when "all"
-        Shop.where(owner_id: user_id).destroy_all
-        Invite.where(user_id: user_id).destroy_all
-        Invite.where(shop_owner_id: user_id).destroy_all
-        User.find(user_id).destroy
-      when "invites"
-        Invite.where(user_id: user_id).destroy_all
-        Invite.where(shop_owner_id: user_id).destroy_all
-      when "shops"
-        Shop.where(owner_id: user_id).destroy_all
+    when 'all'
+      Shop.where(owner_id: user_id).destroy_all
+      Invite.where(user_id: user_id).destroy_all
+      Invite.where(shop_owner_id: user_id).destroy_all
+      User.find(user_id).destroy
+    when 'invites'
+      Invite.where(user_id: user_id).destroy_all
+      Invite.where(shop_owner_id: user_id).destroy_all
+    when 'shops'
+      Shop.where(owner_id: user_id).destroy_all
       end
   end
-  
 end
