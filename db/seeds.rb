8d4837caf7f5b15530786682eb3f592e7c65f7f9
nolyoi <nolan@moonship.cc>
2020-07-10 10:40:53 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+admin = User.create(name: "Nolan", email: "nolyoi210@gmail.com", username: "nolyoi", password: "password", password_confirmation: "password")
+community = admin.communities.create(name: "Bitcoin Community", description: "Post about BTC!")
 
 40.times do
   user = User.create(name: Faker::Name.unique.name, email: Faker::Internet.email, username: Faker::Twitter.unique.screen_name, password: "password", password_confirmation: "password")
