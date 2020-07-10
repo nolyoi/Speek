8a58355de98d38504527fdce9009afcedb9a0ed3
nolyoi <nolan@moonship.cc>
2020-07-10 11:01:49 -0500

    Remove

--- db/migrate/20200707211759_add_public_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPublicToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :private, :boolean, default: false
-  end
-end
--- db/migrate/20200710110748_add_fields_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :uid, :string
-    add_column :users, :provider, :string
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
-  end
-end
